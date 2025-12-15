8e0e6c3a2fb093a4491b8e843537889a7992e103
juanifinanzas-create <juani.finanzas@gmail.com>
2025-12-15 08:18:21 -0300

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+function App() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', padding: '2rem', textAlign: 'center' }}>
+      <h1>¡Villa Tapi App Funciona! 🚀</h1>
+      <p>La aplicación se ha desplegado correctamente en Vercel.</p>
+    </div>
+  )
+}
+export default App
